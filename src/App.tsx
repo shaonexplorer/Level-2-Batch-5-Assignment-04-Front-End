8c2d272663654520895efef8fe1e156b188dc710
shaonexplorer <shaonexplorer@gmail.com>
2025-07-04 17:56:25 +0600

    adding dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
 import { Outlet } from "react-router";
 import NavBar from "./components/nav/navBar";
 import { Toaster } from "sonner";
+import { ThemeProvider } from "./components/theme/theme-provider";
+import { ModeToggle } from "./components/theme/mode-toggle";
 
 function App() {
   return (
-    <div className="w-[1300px] mx-auto">
-      <NavBar />
-      <Toaster position="top-center" />
-      <Outlet />
-    </div>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <div className="w-[1300px] mx-auto">
+        <div className="fex items-center justify-between">
+          <NavBar />
+          <ModeToggle />
+        </div>
+
+        <Toaster position="top-center" />
+        <Outlet />
+      </div>
+    </ThemeProvider>
   );
 }
 
