39dc7c166ded8e22720cbab17e5548b1a2a96a2b
shaonexplorer <shaonexplorer@gmail.com>
2025-07-05 01:17:12 +0600

    apply footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,7 +1,7 @@
 function Footer() {
   return (
     <div className="flex justify-center mb-[20px]">
-      <p className="text-sm sm:text-base">
+      <p className="text-xs sm:text-base">
         &copy; <span>{new Date().getFullYear()}</span> Abir Hasan Khan. All
         Rights Reserved.
         {/* <a href="">Privacy Policy</a> | <a href="">Terms of Service</a> */}
